435c3c4ffd4ca6ce8fb549972cec59a4fdc59d51
KuwaKuwak <77621529+kuWAKuwak@users.noreply.github.com>
2022-03-24 17:47:11 +0900

    Add files via upload

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+var choosedNum int
+
+func main() {
+	for {
+		fmt.Println("0000000000000000000000")
+		fmt.Println("0     Choose Menu    0")
+		fmt.Println("0  1. Write ToDo     0")
+		fmt.Println("0  2. Display ToDo   0")
+		fmt.Println("0  3. Delete ToDo    0")
+		fmt.Println("0  4. Edit ToDO      0")
+		fmt.Println("0  5. Clear ToDo     0")
+		fmt.Println("0  6. Close Menu     0")
+		fmt.Println("0000000000000000000000")
+
+		switch choosedNum {
+		case 1:
+			addToDo(){
+				
+			}
+
+		}
+
+	}
+
+}
